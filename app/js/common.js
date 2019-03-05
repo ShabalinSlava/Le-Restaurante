d34dac14b5bc901a7547438c12f16275ad3b6db9
Shabalin Slava <svaznoy0309@gmail.com>
2019-03-05 14:06:25 +0800

    version 1.0.0

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,27 @@
 $(function() {
+  $("#online-menu-slider").slick({
+    dots: false,
+    arrows: true,
+    appendArrows: $(".online-slider-arrows"),
+    nextArrow: "<button>next offer<div class=arrow></div></button>"
+  });
+  $(".slick-prev").hide();
 
-	// Custom JS
+  $("#menu-button").click(function() {
+    $("#navigation").addClass("visible");
+  });
 
+  $("#navigation-close").click(function() {
+    $("#navigation").removeClass("visible");
+  });
+
+  $("#navigation-block a").click(function() {
+    $("#navigation").removeClass("visible");
+  });
+
+  $("#category-slider").slick({
+    dots: false,
+    arrow: true,
+    appendArrows: $(".category-arrows")
+  });
 });
